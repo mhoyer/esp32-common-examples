92697101ae07b1f025cd5e776eecb871065cf493
Marcel Hoyer <mhoyer@pixelplastic.de>
2024-05-29 18:17:52 +0200

    add blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_PIN 8
+#define LED_ON LOW
+#define LED_OFF HIGH
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LED_PIN, LED_ON);
+  delay(50);
+  digitalWrite(LED_PIN, LED_OFF);
+  delay(950);
 }
