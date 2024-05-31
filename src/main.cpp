aac3401967c38b26512f182d70683f46a85ab71e
Marcel Hoyer <mhoyer@pixelplastic.de>
2024-05-31 17:53:28 +0200

    move blink.loop() into RTOS task

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,23 @@
 #define LED_ON LOW
 #define LED_OFF HIGH
 
+#if CONFIG_FREERTOS_UNICORE
+#define TASK_CORE_ID 0
+#else
+#define TASK_CORE_ID 1
+#endif
+
+void blinkTaskHandler(void *parameter) {
+  while(1)
+  {
+    blink.loop(LED_PIN, INVERT_HIGH_LOW);
+  }
+}
+
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  xTaskCreatePinnedToCore(blinkTaskHandler, "Blink", 1024, NULL, 1, NULL, TASK_CORE_ID);
 }
 
 void loop() {
-  blink.loop(LED_PIN, INVERT_HIGH_LOW);
 }
